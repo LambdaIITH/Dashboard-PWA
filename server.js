74b4b8f644fc856fa1d10cca8d39755e5139b3a2
Bhaskar <bhaskarmandal369@gmail.com>
2024-07-30 03:08:15 +0530

    introducing dark mode and lost-found

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 8080;
 
 app.use(express.static('web'));
 
+app.get('/.well-known/assetlinks.json', (req, res) => {
+  res.sendFile(__dirname + '/web/.well-known/assetlinks.json');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
