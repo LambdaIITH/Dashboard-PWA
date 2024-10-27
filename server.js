822fa3943942043ddf290541e93a2ab0ce5ea171
Bhaskar <bhaskarmandal369@gmail.com>
2024-10-27 19:20:18 +0530

    feat: update app with deep link

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8080;
 
 app.use(express.static('web'));
 
 app.get('/.well-known/assetlinks.json', (req, res) => {
-  res.sendFile(__dirname + '/web/.well-known/assetlinks.json');
+  res.sendFile(path.join(__dirname, 'web/.well-known/assetlinks.json'));
+});
+
+app.get('*', (req, res) => {
+  const originalPath = req.path;
+  res.redirect(`/#${originalPath}`);
 });
 
 app.listen(port, () => {
