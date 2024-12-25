7b6b951f4c76bc8a36cf964f1d3d90e1fae216aa
Bhaskar <bhaskarmandal369@gmail.com>
2024-12-25 16:59:19 +0530

    fix: ui issues

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGwHvbCTCmmZHI1AG_2LW62zIGZl_eAEo",
+  authDomain: "dashboard-iith.firebaseapp.com",
+  projectId: "dashboard-iith",
+  storageBucket: "dashboard-iith.firebasestorage.app",
+  messagingSenderId: "472121739465",
+  appId: "1:472121739465:web:c209e655440be93f9d6518",
+  measurementId: "G-H0YXVRV2BZ"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title || 'Background Message Title';
+  const notificationOptions = {
+    body: payload.notification.body || 'Background Message body.',
+    icon: '/icons/icon-192x192.png',
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
